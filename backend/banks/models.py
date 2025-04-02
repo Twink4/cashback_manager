98586367e78f21736e7985704d9ef4398cd1146c
Twink4 <alex-ander-97@mail.ru>
2025-04-02 11:40:55 +0500

    New models are described

--- backend/banks/models.py
+++ backend/banks/models.py
@@ -1,13 +1,32 @@
 from sqlalchemy import (
     Column,
     Integer,
-    String
+    String,
+    ForeignKey
 )
 
 from backend.core.database import Base
 
+
 class Banks(Base):
     __tablename__ = "banks"
-    
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+
+
+class CashbackCategory(Base):
+    __tablename__ = "cashback_category"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+
+
+class CashbackCategoryWithBank(Base):
+    __tablename__ = "cashback_category_with_bank"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    bank_id = Column(ForeignKey("banks.id"), nullable=False)
+    mcc_code = Column(Integer, nullable=False)
+    cashback_category_id = Column(ForeignKey(
+        "cashback_category.id"), nullable=False)
