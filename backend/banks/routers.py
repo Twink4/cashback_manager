de36620d74a745a4bec93a07e7194aff07139c3c
Twink4 <alex-ander-97@mail.ru>
2025-04-02 08:49:23 +0500

    Checked connection to the database

--- backend/banks/routers.py
+++ backend/banks/routers.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter
 
+from sqlalchemy import select
+
+from ..core.database import DependsSession
+
 router = APIRouter(
     prefix="/banks",
     tags=["Банки"]
 )
 
 @router.get("")
-async def get_banks():
-    return "Bank list"
+async def get_banks(
+    session: DependsSession
+):
+    result = await session.execute(select(1))
+    return result.scalar()
