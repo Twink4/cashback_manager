d3ef52f3803bbb6e0d5487999e7cbeb981839fc9
Twink4 <alex-ander-97@mail.ru>
2025-04-11 11:09:25 +0500

    routes.py renamed routers.py, add routers for cashback categories

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import uvicorn
 
 from fastapi import FastAPI
 
-from backend.banks.routes import router as banks_routes
-from backend.mccs.routes import router as mccs_routes
+from backend.banks.routers import router as banks_routers
+from backend.mccs.routers import router as mccs_routers
+from backend.mccs.routers import router as cashback_category_routers
 
 app = FastAPI(
     title="Cashback Manager"
 )
 
-app.include_router(banks_routes)
-app.include_router(mccs_routes)
+app.include_router(banks_routers)
+app.include_router(mccs_routers)
+app.include_router(cashback_category_routers)
+
 
 if __name__ == "__main__":
     uvicorn.run(
